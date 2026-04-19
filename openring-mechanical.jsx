063e1b79e0c9aa73bd89bb9cd3e988476b74d340
bitcoinkook <andreutio@gmail.com>
2026-04-19 01:40:56 +0100

    Update openring-mechanical.jsx

--- openring-mechanical.jsx
+++ openring-mechanical.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+const { useState, useRef, useEffect } = React;
 
 // ─── CONFIG ───────────────────────────────────────────────────────────────────
 
